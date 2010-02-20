76b86cdf2a4e6b7d38c06b021c12e30f64446361
John Flatness <john@zerocrates.org>
2010-02-20 02:53:40 +0000

    Adds flag for full/id-only output options for record nodes.

--- libraries/Export/OmekaXmlWriter/Record.php
+++ libraries/Export/OmekaXmlWriter/Record.php
@@ -3,10 +3,12 @@
 abstract class Export_OmekaXmlWriter_Record extends Export_OmekaXmlWriter_Node
 {   
     protected $record;
+    protected $fullOutput;
     
-    public function __construct($writer, $record)
+    public function __construct($writer, $record, $fullOutput = true)
     {
         parent::__construct($writer);
         $this->record = $record;
+        $this->fullOutput = $fullOutput;
     }
 }
