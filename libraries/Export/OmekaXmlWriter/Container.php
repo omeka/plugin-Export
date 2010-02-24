fe8687e9f3502857087e9dd07f6c42ab518a96e0
John Flatness <john@zerocrates.org>
2010-02-24 08:07:53 +0000

    Adds ability to pass a specific array of records to a collection node.

--- libraries/Export/OmekaXmlWriter/Container.php
+++ libraries/Export/OmekaXmlWriter/Container.php
@@ -2,6 +2,14 @@
 
 abstract class Export_OmekaXmlWriter_Container extends Export_OmekaXmlWriter_Node
 {
+    protected $records;
+    
+    public function __construct($writer, $records = null)
+    {
+        parent::__construct($writer);
+        $this->records = $records;
+    }
+    
     public function writeNode()
     {
         $this->startContainer();
@@ -17,6 +25,7 @@ abstract class Export_OmekaXmlWriter_Container extends Export_OmekaXmlWriter_Nod
     protected function endContainer()
     {
         $this->writer->endElement();
+        $this->writer->flush();
     }
     
     abstract protected function writeContents();
